4bf12c2172b2d1ec9398836638001350071f5e99
Paul Barrick <peb7268@gmail.com>
2013-10-08 17:18:20 -0400

    styles and header tweaks

--- wp-content/themes/intengo/header.php
+++ wp-content/themes/intengo/header.php
@@ -14,70 +14,73 @@
 </head>
 <body <?php body_class();?>>
 <!-- <div id="pageWrapper"> -->
-	<header class="clearfix">
-		<div class="inner">
-			<a href="<?php bloginfo('url'); ?>">
-				<img src="<?php echo IMAGES_DIR; ?>/logo.png" id="logo" />
-			</a>
-			<a href="#" id="navControl" class="clearfix">
-				<div class="one">▔</div>
-				<div class="two">▔</div>
-				<div class="three">▔</div>
-			</a>
-			<ul id="nav" class="clearfix">
-				<li id="home">
-					<a href="http://gointengo.com/?page_id=5">Home</a>
-					<span class="seperator"></span>
-					<ul class="sub-nav">
-						<li><a href="#block1">Superior Prediction Markets</a></li>
-						<li><a href="#cta-home1">Get Inspired</a></li>
-					</ul>
-				</li>
-				<li id="solutions">
-					<span class="seperator"></span>
-					<a href="http://gointengo.com/?page_id=9">Solutions</a>
-					<ul class="sub-nav">
-						<li><a href="#parallax1">Product Concepts</a></li>
-						<li><a href="#cta-solutions2">Brand Positioning &amp; Creative</a></li>
-					</ul>
-				</li>
-				<li id="methodology">
-					<span class="seperator"></span>
-					<a href="http://gointengo.com/?page_id=14">Methodology</a>
-					<ul class="sub-nav">
-						<li><a href="#parallax1">Premise</a></li>
-						<li><a href="#cta-methodology1">How It Works</a></li>
-						<li><a href="#cta-methodology2">A pure approach</a></li>
-						<li><a href="#cta-methodology3">Recruiting Participants</a></li>
-						<li><a href="#cta-methodology4">Confidentiality</a></li>
-					</ul>
-				</li>
-				<li id="case-study">
-					<span class="seperator"></span>
-					<a href="http://gointengo.com/?page_id=16">Case Study</a>
-					<ul class="sub-nav">
-						<li><a href="#parallax1">Two Paths For Screening</a></li>
-						<li><a href="#infographic">The Big Picture</a></li>
-					</ul>
-				</li>
-				<li id="about" class="hidden">
-					<span class="seperator"></span>
-					<a href="http://gointengo.com/?page_id=84">About</a>
-				</li>
-				<li id="contactUs" class="hidden">
-					<span class="seperator"></span>
-					<a href="http://gointengo.com/?page_id=87">Contact Us</a>
-				</li>
-			</ul>
-		</div>
-	</header>
+	<div id="headerWrapper">
+		<header class="clearfix">
+			<div class="inner">
+				<a href="<?php bloginfo('url'); ?>">
+					<img src="<?php echo IMAGES_DIR; ?>/logo.png" id="logo" />
+				</a>
+				<a href="#" id="navControl" class="clearfix">
+					<div class="one">▔</div>
+					<div class="two">▔</div>
+					<div class="three">▔</div>
+				</a>
+				<ul id="nav" class="clearfix">
+					<li id="home">
+						<a href="http://gointengo.com/?page_id=5">Home</a>
+						<span class="seperator"></span>
+						<ul class="sub-nav">
+							<li><a href="#block1">Superior Prediction Markets</a></li>
+							<li><a href="#cta-home1">Get Inspired</a></li>
+						</ul>
+					</li>
+					<li id="solutions">
+						<span class="seperator"></span>
+						<a href="http://gointengo.com/?page_id=9">Solutions</a>
+						<ul class="sub-nav">
+							<li><a href="#parallax1">Product Concepts</a></li>
+							<li><a href="#cta-solutions2">Brand Positioning &amp; Creative</a></li>
+						</ul>
+					</li>
+					<li id="methodology">
+						<span class="seperator"></span>
+						<a href="http://gointengo.com/?page_id=14">Methodology</a>
+						<ul class="sub-nav">
+							<li><a href="#parallax1">Premise</a></li>
+							<li><a href="#cta-methodology1">How It Works</a></li>
+							<li><a href="#cta-methodology2">A pure approach</a></li>
+							<li><a href="#cta-methodology3">Recruiting Participants</a></li>
+							<li><a href="#cta-methodology4">Confidentiality</a></li>
+						</ul>
+					</li>
+					<li id="case-study">
+						<span class="seperator"></span>
+						<a href="http://gointengo.com/?page_id=16">Case Study</a>
+						<ul class="sub-nav">
+							<li><a href="#parallax1">Two Paths For Screening</a></li>
+							<li><a href="#infographic">The Big Picture</a></li>
+						</ul>
+					</li>
+					<li id="about" class="hidden">
+						<span class="seperator"></span>
+						<a href="http://gointengo.com/?page_id=84">About</a>
+					</li>
+					<li id="contactUs" class="hidden">
+						<span class="seperator"></span>
+						<a href="http://gointengo.com/?page_id=87">Contact Us</a>
+					</li>
+				</ul>
+			</div>
+		</header>
+	</div><!-- #headerWrapper -->
+
 	<div id="nav2" class="wrapper">
 		<div id="secondaryNav">
 			<ul id="socialIcons">
-				<li><a href="#facebook">&nbsp;</a></li>
-				<li><a href="#twitter">&nbsp;</a></li>
-				<li><a href="#youtube">&nbsp;</a></li>
-				<li class="last"><a href="#linkedin">&nbsp;</a></li>
+				<li><a href="https://www.facebook.com/intengo">&nbsp;</a></li>
+				<li><a href="www.twitter.com/gointengo">&nbsp;</a></li>
+				<li><a href="http://www.youtube.com/channel/UCNAfn8gqP5poB8q9HkJmpNw">&nbsp;</a></li>
+				<li class="last"><a href="http://www.linkedin.com/company/intengo">&nbsp;</a></li>
 			</ul>
 			<a href="http://gointengo.com/?page_id=84" id="about"></a>
 			<a href="http://gointengo.com/?page_id=87" id="contact-us"></a>
