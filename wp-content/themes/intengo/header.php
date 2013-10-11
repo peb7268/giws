ce16f6a6b6e5484307cc41c70e1245408bd319b3
Paul Barrick <peb7268@gmail.com>
2013-10-11 16:12:07 -0400

    header changes

--- wp-content/themes/intengo/header.php
+++ wp-content/themes/intengo/header.php
@@ -27,45 +27,45 @@
 				</a>
 				<ul id="nav" class="clearfix">
 					<li id="home">
-						<a href="http://gointengo.com/home">Home</a>
+						<a href="http://demo.gointengo.com/home">Home</a>
 						<span class="seperator"></span>
 						<ul class="sub-nav">
-							<li><a href="http://gointengo.com/home#block1">Superior Prediction Markets</a></li>
-							<li><a href="http://gointengo.com/home#cta-home1">Get Inspired</a></li>
+							<li><a href="http://demo.gointengo.com/home#block1">Superior Prediction Markets</a></li>
+							<li><a href="http://demo.gointengo.com/home#cta-home1">Get Inspired</a></li>
 						</ul>
 					</li>
 					<li id="solutions">
 						<span class="seperator"></span>
-						<a href="http://gointengo.com/solutions">Solutions</a>
+						<a href="http://demo.gointengo.com/solutions">Solutions</a>
 						<ul class="sub-nav">
-							<li><a href="http://gointengo.com/solutions#cta-solutions1">Product Concepts</a></li>
-							<li><a href="http://gointengo.com/solutions#cta-solutions2">Brand Positioning &amp; Creative</a></li>
+							<li><a href="http://demo.gointengo.com/solutions#cta-solutions1">Product Concepts</a></li>
+							<li><a href="http://demo.gointengo.com/solutions#cta-solutions2">Brand Positioning &amp; Creative</a></li>
 						</ul>
 					</li>
 					<li id="methodology">
 						<span class="seperator"></span>
-						<a href="http://gointengo.com/methodology">Methodology</a>
+						<a href="http://demo.gointengo.com/methodology">Methodology</a>
 						<ul class="sub-nav">
-							<li><a href="http://gointengo.com/methodology#parallax1">Premise</a></li>
-							<li><a href="http://gointengo.com/methodology#cta-methodology1">How It Works</a></li>
-							<li><a href="http://gointengo.com/methodology#cta-methodology2">A pure approach</a></li>
-							<li><a href="http://gointengo.com/methodology#cta-methodology3">Recruiting Participants</a></li>
-							<li><a href="http://gointengo.com/methodology#cta-methodology4">Confidentiality</a></li>
+							<li><a href="http://demo.gointengo.com/methodology#parallax1">Premise</a></li>
+							<li><a href="http://demo.gointengo.com/methodology#cta-methodology1">How It Works</a></li>
+							<li><a href="http://demo.gointengo.com/methodology#cta-methodology2">A pure approach</a></li>
+							<li><a href="http://demo.gointengo.com/methodology#cta-methodology3">Recruiting Participants</a></li>
+							<li><a href="http://demo.gointengo.com/methodology#cta-methodology4">Confidentiality</a></li>
 						</ul>
 					</li>
 					<li id="case-study">
 						<span class="seperator"></span>
-						<a href="http://gointengo.com/case-study">Case Study</a>
+						<a href="http://demo.gointengo.com/case-study">Case Study</a>
 						<ul class="sub-nav">
-							<li><a href="http://gointengo.com/case-study#parallax1">Two Paths For Screening</a></li>
-							<li><a href="http://gointengo.com/case-study#infographic">The Big Picture</a></li>
+							<li><a href="http://demo.gointengo.com/case-study#parallax1">Two Paths For Screening</a></li>
+							<li><a href="http://demo.gointengo.com/case-study#infographic">The Big Picture</a></li>
 						</ul>
 					</li>
 					<li id="about" class="hidden">
-						<a href="http://gointengo.com/about">About</a>
+						<a href="http://demo.gointengo.com/about">About</a>
 					</li>
 					<li id="contactUs" class="hidden">
-						<a href="http://gointengo.com/contact-us">Contact Us</a>
+						<a href="http://demo.gointengo.com/contact-us">Contact Us</a>
 					</li>
 				</ul>
 			</div>
@@ -80,8 +80,8 @@
 				<li><a href="http://www.youtube.com/channel/UCNAfn8gqP5poB8q9HkJmpNw">&nbsp;</a></li>
 				<li class="last"><a href="http://www.linkedin.com/company/intengo">&nbsp;</a></li>
 			</ul>
-			<a href="http://gointengo.com/about" id="about"></a>
-			<a href="http://gointengo.com/contact-us" id="contact-us"></a>
+			<a href="http://demo.gointengo.com/about" id="about"></a>
+			<a href="http://demo.gointengo.com/contact-us" id="contact-us"></a>
 			<p class="tagline">Explore Your Possibilities</p>
 			<p class="phone">855.844.3172</p>
 		</div>
